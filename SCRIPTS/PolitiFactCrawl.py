5875a1627f9ab221efb3f20d9ece12308317d171
Andrew Lin <65137210+AndrewLin0406@users.noreply.github.com>
2026-05-28 18:59:35 -0400

    Added scripts

---
+++ SCRIPTS/PolitiFactCrawl.py
@@ -0,0 +1,571 @@
+"""
+Article Crawl on PolitiFact website.
+"""
+
+import requests, json, argparse, time
+from pathlib import Path
+from datetime import datetime
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+
+#============================================== HELPERS ==============================================
+
+URLs = {
+    "main": "https://www.politifact.com",
+    "latest": "https://www.politifact.com/factchecks/list/",
+}
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0"
+}
+
+verdicts = {
+    "barely-true": "Mostly false",
+    "half-true": "Half true",
+    "mostly-true": "Mostly true",
+    "true": "True",
+    "false": "False",
+    "pants-fire": "Pants on fire"
+}
+
+#============================================== Function ==============================================
+
+def parse_date(date):
+
+    cleaned_date = (
+        date
+        .split("(")[0]
+        .replace(".", "")
+        .replace("Sept", "Sep")
+        .strip()
+    )
+
+    formats = [
+        "%b %d, %Y",
+        "%B %d, %Y"
+    ]
+
+    for fmt in formats:
+
+        try:
+            return datetime.strptime(
+                cleaned_date,
+                fmt
+            )
+
+        except ValueError:
+            continue
+
+    raise ValueError(
+        f"Unknown date format: {date}"
+    )
+
+def getArticles(timestamp=None, limit=-1):
+
+    done = False
+
+    articles = []
+
+    timestamp_datetime = None
+
+    if timestamp is not None:
+
+        timestamp_datetime = datetime.strptime(
+            str(timestamp),
+            "%Y%m%d"
+        )
+
+    page = 1
+
+    while True:
+
+        page_url = (
+            f"{URLs['latest']}?page={page}"
+        )
+
+        # print(f"Collecting page {page}")
+
+        response = requests.get(
+            page_url,
+            headers=HEADERS,
+            timeout=10
+        )
+
+        response.raise_for_status()
+
+        soup = BeautifulSoup(
+            response.text,
+            "html.parser"
+        )
+
+        cards = soup.find_all(
+            "article",
+            class_="m-statement"
+        )
+
+        if not cards:
+            print("No more articles.")
+            break
+
+        for card in cards:
+
+            try:
+
+                # =================================
+                # URL
+                # =================================
+
+                quote_tag = card.find(
+                    "div",
+                    class_="m-statement__quote"
+                )
+
+                a_tag = (
+                    quote_tag.find("a", href=True)
+                    if quote_tag else None
+                )
+
+                article_url = (
+                    urljoin(
+                        URLs["main"],
+                        a_tag["href"]
+                    )
+                    if a_tag else None
+                )
+
+                # =================================
+                # CLAIM
+                # =================================
+
+                # claim = (
+                #     quote_tag.get_text(
+                #         " ",
+                #         strip=True
+                #     )
+                #     if quote_tag else None
+                # )
+
+                # =================================
+                # RATING
+                # =================================
+
+                # rating_tag = card.find(
+                #     "img",
+                #     class_="c-image__original"
+                # )
+
+                # rating = (
+                #     rating_tag.get("alt")
+                #     if rating_tag else None
+                # )
+
+                # =================================
+                # SPEAKER
+                # =================================
+
+                speaker_tag = card.find(
+                    "a",
+                    class_="m-statement__name"
+                )
+
+                speaker = (
+                    speaker_tag.get_text(
+                        strip=True
+                    )
+                    if speaker_tag else None
+                )
+
+                # =================================
+                # DATE
+                # =================================
+
+                footer_tag = card.find(
+                    "footer",
+                    class_="m-statement__footer"
+                )
+
+                date = None
+
+                if footer_tag:
+
+                    footer_text = footer_tag.get_text(
+                        " ",
+                        strip=True
+                    )
+
+                    # Example:
+                    # By Caleb McCullough • May 27, 2026
+
+                    if "•" in footer_text:
+
+                        date = (
+                            footer_text
+                            .split("•")[-1]
+                            .strip()
+                        )
+
+                # =================================
+                # FILTER BY TIMESTAMP
+                # =================================
+
+                article_datetime = None
+
+                if date:
+
+                    article_datetime = parse_date(
+                        date
+                    )
+
+                if (
+                    timestamp_datetime is not None
+                    and article_datetime is not None
+                ):
+
+                    if article_datetime < timestamp_datetime:
+                        done = True
+                        break
+
+                # =================================
+                # ARTICLE OBJECT
+                # =================================
+
+                article = {
+                    # "claim": claim,
+                    # "rating": rating,
+                    "speaker": speaker,
+                    "date": date,
+                    "article_url": article_url
+                }
+
+                articles.append(article)
+
+                #print(claim)
+
+            except Exception as e:
+
+                print("FAILED:", e)
+
+        page += 1
+
+        if (limit != -1 and page > limit) or done:
+            break
+
+        time.sleep(1)
+
+    return articles
+
+def scrape_article(url):
+    response = requests.get(
+        url,
+        headers=HEADERS,
+        timeout=10
+    )
+
+    response.raise_for_status()
+
+    soup = BeautifulSoup(
+        response.text,
+        "html.parser"
+    )
+
+    article_data = {
+        "claim": None,
+        "rating": None,
+        "speaker": None,
+        "author": None,
+        "content": "",
+        "sources": []
+    }
+
+    # ======================================
+    # CLAIM
+    # ======================================
+
+    claim_tag = soup.find(
+        "div",
+        class_="m-statement__quote"
+    )
+
+    if claim_tag:
+
+        article_data["claim"] = claim_tag.get_text(
+            " ",
+            strip=True
+        )
+
+    # ======================================
+    # RATING
+    # ======================================
+
+    meter_div = soup.find(
+        "div",
+        class_="m-statement__meter"
+    )
+
+    if meter_div:
+
+        rating_tag = meter_div.find(
+            "img",
+            alt=True
+        )
+
+        if rating_tag:
+
+            article_data["rating"] = verdicts[rating_tag.get("alt")]
+
+    # ======================================
+    # SPEAKER
+    # ======================================
+
+    speaker_tag = soup.find(
+        "a",
+        class_="m-statement__name"
+    )
+
+    if speaker_tag:
+
+        article_data["speaker"] = speaker_tag.get_text(
+            " ",
+            strip=True
+        )
+
+    # ======================================
+    # AUTHORS + DATE
+    # ======================================
+
+    author_blocks = soup.find_all(
+        "div",
+        class_="m-author__content"
+    )
+
+    authors = []
+
+    for block in author_blocks:
+
+        author_link = block.find("a")
+
+        date_tag = block.find(
+            "span",
+            class_="m-author__date"
+        )
+
+        author_name = (
+            author_link.get_text(
+                " ",
+                strip=True
+            )
+            if author_link else None
+        )
+
+        article_data["author"] = author_name
+
+    # ======================================
+    # CONTENT
+    # ======================================
+
+    content = {
+        "if_your_time_is_short": [],
+        "article_content": [],
+        "our_ruling": []
+    }
+
+    body = soup.find(
+        "article",
+        class_="m-textblock"
+    )
+
+    if body:
+
+        # ==================================
+        # FULL ARTICLE CONTENT
+        # ==================================
+
+        for p in body.find_all("p"):
+
+            text = p.get_text(
+                " ",
+                strip=True
+            )
+
+            if text:
+
+                content["article_content"].append(
+                    text
+                )
+
+        # ==================================
+        # IF YOUR TIME IS SHORT
+        # ==================================
+
+        short_section = soup.find(
+            "div",
+            class_="short-on-time"
+        )
+
+        if short_section:
+
+            for li in short_section.find_all("li"):
+
+                text = li.get_text(
+                    " ",
+                    strip=True
+                )
+
+                if text:
+
+                    content[
+                        "if_your_time_is_short"
+                    ].append(text)
+                    
+        # ==================================
+        # OUR RULING
+        # ==================================
+
+        ruling_header = body.find(
+            lambda tag:
+                tag.name in ["h2", "h3"]
+                and "our ruling"
+                in tag.get_text().lower()
+        )
+
+        if ruling_header:
+
+            current = ruling_header.find_next_sibling()
+
+            while current:
+
+                if current.name in ["h2", "h3"]:
+                    break
+
+                if current.name == "p":
+
+                    text = current.get_text(
+                        " ",
+                        strip=True
+                    )
+
+                    if text:
+
+                        content[
+                            "our_ruling"
+                        ].append(text)
+
+                current = current.find_next_sibling()
+
+    article_data["content"] = content
+
+    # ======================================
+    # SOURCES
+    # ======================================
+
+    sources_section = soup.find(
+        "section",
+        id="sources"
+    )
+
+    sources = []
+
+    if sources_section:
+
+        source_paragraphs = sources_section.find_all("p")
+
+        for p in source_paragraphs:
+
+            source = {
+                "raw_text": p.get_text(
+                    " ",
+                    strip=True
+                ),
+                "links": None
+            }
+
+            for a in p.find_all(
+                "a",
+                href=True
+            ):
+
+                source["links"] = a["href"]
+
+            sources.append(source)
+
+    article_data["sources"] = sources
+
+    return article_data
+
+def getData(outputDirectory, category, timeStamp, limit, dataset_name="politifact_articles.json"):
+    outputDirectory.mkdir(
+        parents=True,
+        exist_ok=True
+    )
+
+    articles = getArticles(timeStamp, limit=limit)
+    outputFile = outputDirectory / dataset_name
+
+    filtered_articles = []
+
+    for article in articles:
+
+        article_data = scrape_article(
+            article["article_url"]
+        )
+
+        # skip non-English articles
+        if article_data is None:
+            continue
+
+        article["article_data"] = article_data
+
+        filtered_articles.append(article)
+    
+    with open(outputFile, "w", encoding="utf-8") as f:
+        json.dump(
+            filtered_articles,
+            f,
+            ensure_ascii=False,
+            indent=4
+        )
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description=(
+            "Extract the articles from the Snopes.com web"
+        )
+    )
+    parser.add_argument(
+        "--output-dir",
+        type=Path,
+        default=Path("DATASET"),
+        help="Root directory containing extracted datasets.",
+    )
+    parser.add_argument(
+        "--category",
+        type=str,
+        default="fact-check",
+        help="Category of articles (ALL, latest, trending, politics, entertainment).",
+    )
+    parser.add_argument(
+        "--time-stamp",
+        type=int,
+        default=None,
+        help="Format : (YYYYMMDD), Leave None if do not want to filter by time"
+    )
+    parser.add_argument(
+        "--limit",
+        type=int,
+        default=200,
+        help="Maximum number of pages to crawl",
+    )
+    return parser.parse_args()
+
+def main() -> None:
+    args = parse_args()
+
+    output_root = args.output_dir
+    category_name = args.category
+    time_stamp = args.time_stamp
+    limit = args.limit
+
+    getData(output_root, category_name, time_stamp, limit)
+
+if __name__ == "__main__":
+    main()
---
+++ SCRIPTS/SnopesCrawl.py
@@ -0,0 +1,371 @@
+"""
+Article Crawl on snopes.com website.
+"""
+
+import requests, json, time, argparse
+from pathlib import Path
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+from datetime import datetime
+
+#============================================== HELPERS ==============================================
+
+URLs = {
+    "main": "https://www.snopes.com/",
+    "fact-check": "https://www.snopes.com/fact-check/",
+    "trending": "https://www.snopes.com/top/",
+    "latest": "https://www.snopes.com/top/",
+    "politics": "https://www.snopes.com/category/politics/",
+    "entertainment": "https://www.snopes.com/category/Entertainment/"
+}
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0"
+}
+
+#============================================== Function ==============================================
+
+from datetime import datetime
+
+def parse_date(date):
+
+    cleaned_date = (
+        date
+        .split("(")[0]
+        .replace(".", "")
+        .replace("Sept", "Sep")
+        .strip()
+    )
+
+    formats = [
+        "%b %d, %Y",
+        "%B %d, %Y"
+    ]
+
+    for fmt in formats:
+
+        try:
+            return datetime.strptime(
+                cleaned_date,
+                fmt
+            )
+
+        except ValueError:
+            continue
+
+    raise ValueError(
+        f"Unknown date format: {date}"
+    )
+
+def getArticles(URL_category="fact-check", timestamp=None, limit=-1):
+
+    articles = []
+    page = 1
+
+    done = False
+
+    timestamp_datetime = None
+
+    if timestamp is not None:
+
+        timestamp_datetime = datetime.strptime(
+            str(timestamp),
+            "%Y%m%d"
+        )
+
+    while True:
+
+        if page == 1:
+            page_url = URLs[URL_category]
+        else:
+            page_url = f"{URLs[URL_category]}?pagenum={page}"
+
+        # print(
+        #     "-" * 10 +
+        #     f" Collecting Articles from Page {page} " +
+        #     "-" * 10,
+        #     end="\n\n"
+        # )
+
+        html = requests.get(page_url).text
+        soup = BeautifulSoup(html, "html.parser")
+
+        article_wrappers = soup.find_all(
+            "div",
+            class_="article_wrapper"
+        )
+
+        if not article_wrappers:
+            print("No more articles found.")
+            break
+
+        for wrapper in article_wrappers:
+
+            try:
+
+                # URL
+                a_tag = wrapper.find(
+                    "a",
+                    class_="outer_article_link_wrapper"
+                )
+
+                article_url = a_tag["href"]
+                article_url = article_url.replace(".com//", ".com/")
+
+                # TITLE
+                title_tag = wrapper.find(
+                    "h3",
+                    class_="article_title"
+                )
+
+                title = title_tag.get_text(strip=True)
+
+                # AUTHOR
+                author_tag = wrapper.find(
+                    "span",
+                    class_="author_name"
+                )
+
+                author = (
+                    author_tag.get_text(strip=True)
+                    if author_tag else None
+                )
+
+                # DATE
+                date_tag = wrapper.find(
+                    "span",
+                    class_="article_date"
+                )
+
+                date = (
+                    date_tag.get_text(strip=True)
+                    if date_tag else None
+                )
+                
+                article_datetime = None
+
+                if date:
+                    article_datetime = parse_date(date)
+
+                # BYLINE
+                byline_tag = wrapper.find(
+                    "span",
+                    class_="article_byline"
+                )
+
+                byline = (
+                    byline_tag.get_text(" ", strip=True)
+                    if byline_tag else None
+                )
+
+                # SINGLE ARTICLE OBJECT
+                article = {
+                    "title": title,
+                    "article_url": article_url,
+                    "author": author,
+                    "date": date,
+                    "byline": byline
+                }
+
+                if (timestamp_datetime is not None and article_datetime is not None):
+                    print(article_datetime, timestamp_datetime)
+                    if article_datetime < timestamp_datetime:
+                        print("2")
+                        done = True
+                        break   
+
+                articles.append(article)
+
+            except Exception as e:
+                print("Failed:", e)
+
+        page += 1
+
+        if (limit != -1 and page > limit) or done:
+            print("Finished collecting articles")
+            break
+    
+    return articles
+
+def scrape_article(url):
+
+    html = requests.get(url, headers=HEADERS).text
+    soup = BeautifulSoup(html, "html.parser")
+
+    article_data = {
+        "claim": None,
+        "rating": None,
+        "author": None,
+        "datePublished": None,
+        "content": "",
+        "sources": []
+    }
+
+    # ==========================================
+    # PARSE JSON-LD
+    # ==========================================
+
+    scripts = soup.find_all(
+        "script",
+        type="application/ld+json"
+    )
+
+    for script in scripts:
+
+        try:
+            data = json.loads(script.string)
+
+            # ----------------------------
+            # ClaimReview block
+            # ----------------------------
+            if data.get("@type") == "ClaimReview":
+
+                article_data["claim"] = data.get(
+                    "claimReviewed"
+                )
+
+                review = data.get("reviewRating", {})
+
+                article_data["rating"] = review.get(
+                    "alternateName"
+                )
+
+            # ----------------------------
+            # Article block
+            # ----------------------------
+            elif data.get("@type") == "Article":
+                article_data["datePublished"] = data.get(
+                    "datePublished"
+                )
+
+                author = data.get("author", {})
+
+                article_data["author"] = author.get(
+                    "name"
+                )
+
+        except Exception:
+            pass
+
+    # ==========================================
+    # ARTICLE CONTENT
+    # ==========================================
+
+    article_body = soup.find("article")
+
+    paragraphs = []
+
+    if article_body:
+
+        for p in article_body.find_all("p"):
+
+            text = p.get_text(" ", strip=True)
+
+            if text:
+                paragraphs.append(text)
+
+    article_data["content"] = "\n\n".join(paragraphs)
+
+    # ==========================================
+    # HYPERLINK SOURCES : PROBABLY DONT WANT THIS
+    # ==========================================
+
+    # links = []
+
+    # if article_body:
+
+    #     for a in article_body.find_all("a", href=True):
+
+    #         href = a["href"]
+    #         text = a.get_text(" ", strip=True)
+
+    #         links.append({
+    #             "text": text,
+    #             "url": href
+    #         })
+
+    # article_data["hyper_links"] = links[1:]
+
+    # ==========================================
+    # SOURCES / REFERENCES
+    # ==========================================
+
+    sources = []
+
+    sources_div = soup.find("div", id="sources_rows")
+
+    if sources_div:
+
+        for p in sources_div.find_all("p"):
+
+            source = []
+
+            sources.append(p.get_text(
+                " ",
+                strip=True
+            ))
+
+    article_data["sources"] = sources
+
+    return article_data
+
+def getData(outputDirectory, category, timeStamp, limit, dataset_name="snopes_articles.json"):
+    outputDirectory.mkdir(
+        parents=True,
+        exist_ok=True
+    )
+
+    articles = getArticles(category, timeStamp, limit=limit)
+    outputFile = outputDirectory / dataset_name
+
+    for article in articles:
+        article_data = scrape_article(article["article_url"])
+        article["article_data"] = article_data
+    
+    with open(outputFile, "w", encoding="utf-8") as f:
+        json.dump(articles, f, ensure_ascii=False, indent=4)
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description=(
+            "Extract the articles from the Snopes.com web"
+        )
+    )
+    parser.add_argument(
+        "--output-dir",
+        type=Path,
+        default=Path("DATASET"),
+        help="Root directory containing extracted datasets.",
+    )
+    parser.add_argument(
+        "--category",
+        type=str,
+        default="fact-check",
+        help="Category of articles (ALL, latest, trending, politics, entertainment).",
+    )
+    parser.add_argument(
+        "--time-stamp",
+        type=int,
+        default=None,
+        help="Format : (YYYYMMDD), Leave None if do not want to filter by time"
+    )
+    parser.add_argument(
+        "--limit",
+        type=int,
+        default=300,
+        help="Maximum number of pages to crawl",
+    )
+    return parser.parse_args()
+
+def main() -> None:
+    args = parse_args()
+
+    output_root = args.output_dir
+    category_name = args.category
+    time_stamp = args.time_stamp
+    limit = args.limit
+
+    getData(output_root, category_name, time_stamp, limit)
+
+if __name__ == "__main__":
+    main()
